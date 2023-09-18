67bdfd52089e5f2a8a4fbde528ef8a4c1d7383f6
Ana Flores <ana.silva87@estudante.ifms.edu.br>
2023-09-18 19:43:02 -0400

    register.js

--- src/app/pages/register/register.js
+++ src/app/pages/register/register.js
@@ -3,9 +3,28 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 
-const Formulario = () => {
+const Telalogin = () => {
 
     const handlerLogin = async (e) => {
       e.preventDefault();
       toast.success('registrado com sucesso')
     }
+
+    return (
+        <div>
+          <h1>Login</h1>
+          <form onSubmit={handlerLogin}>
+    
+          <input
+              placeholder='Nome'
+              type="nome" >
+            </input>
+            <input
+        
+              placeholder='E-mail'
+              type="email">
+            </input> </div>
+  )
+};
+
+export default Telalogin;
