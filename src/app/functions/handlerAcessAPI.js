b9f8675ee1068eb43fce466c018a815a62196b68
Ana Flores <ana.silva87@estudante.ifms.edu.br>
2023-09-18 22:18:12 -0400

    crianção da const Users

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,5 +1,24 @@
 'use server'
 
+const Users = [{
+    nome: 'flores',
+    email:'flores@gmail.com',
+    senha:'123456',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},{
+    nome: 'ana',
+    email:'ana@gmail.com',
+    senha:'123456',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},{
+    nome: 'carla',
+    email:'carla@gmail.com',
+    senha:'123456',
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+}
+
+];
+
 const getUserAuthenticated = async (userLogin) => {
 let userAuth = {};
 
