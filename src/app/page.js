b51cea16eef813eb55020f99156c9ce1725ce4cf
Ana Flores <ana.silva87@estudante.ifms.edu.br>
2023-09-18 22:57:59 -0400

    import e const userAuth

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,31 @@
 import { useState } from "react";
 import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Login() {
   const [user, setUser] = useState({
     email: '',
     password: '',
   });
+
   const { push, refresh } = useRouter();
 
   const handlerLogin = async (e) => {
     e.preventDefault();
+
     try {
-      await handlerAcessUser(user);
+      const userAuth = await handlerAcessUser(user);
+      if(userAuth.token === undefined){
+        toast.error("ERRO! EMAIL OU SENHA")
+      }
       push('/pages/dashboard');
     } catch {
       refresh();
     }
   }
+
   return (
     <div>
       <h1>Login</h1>
